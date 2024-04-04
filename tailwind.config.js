be3aceb566abb116ae29a7abc84694bdc086e35e
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-04-04 13:00:20 +0200

    feat: tailwind intro

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "wa-blue": "#184689",
+      },
+    },
+  },
+  plugins: [],
+};
