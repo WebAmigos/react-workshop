19525bdaa7ec57f30907d68d2e3a4758003266ff
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-04-04 20:43:25 +0200

    feat: events and types

--- src/components/Generator/Generator.tsx
+++ src/components/Generator/Generator.tsx
@@ -1,17 +1,23 @@
-import { useState } from "react";
+import { type MouseEventHandler, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { Button, Text } from "../../ui";
 
 export const Generator = () => {
   const [id, setId] = useState(uuidv4());
 
-  const handleClick = () => {
+  const handleClick: MouseEventHandler<HTMLButtonElement> = () => {
     // id = uuidv4();
     setId(uuidv4());
   };
 
+  const handleDivClick: MouseEventHandler<HTMLDivElement> = () => {
+    // id = uuidv4();
+    // setId(uuidv4());
+    console.log("div click!");
+  };
+
   return (
-    <div>
+    <div onClick={handleDivClick}>
       <Text>{id}</Text>
       <Button label="Refresh" onClick={handleClick} />
     </div>
