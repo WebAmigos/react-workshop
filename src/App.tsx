0074fdb5e1ce6609b66bde935358b20cddd7ca49
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-04-13 21:26:31 +0200

    feat: 3.11 error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,28 @@ import { RouterProvider } from "react-router-dom";
 
 import { AuthContextProvider } from "./components/Auth/AuthContext";
 import { ThemeContextProvider } from "./components/Theme/ThemeContext";
+import { AuthInfo } from "./components/Auth/AuthInfo";
 import { router } from "./routes";
 
 import "./App.css";
+import { ErrorBoundary } from "./components/ErrorBoundary";
 
 function App() {
   return (
     <>
-      <ThemeContextProvider>
-        {/* <ThemeSwitcher /> */}
-        <AuthContextProvider>
-          <RouterProvider router={router} />
-          {/* <AuthInfo /> */}
-        </AuthContextProvider>
-      </ThemeContextProvider>
+      <ErrorBoundary
+        fallback={<p className="dark:text-slate-300">General error</p>}
+      >
+        <ThemeContextProvider>
+          {/* <ThemeSwitcher /> */}
+          <AuthContextProvider>
+            <RouterProvider router={router} />
+          </AuthContextProvider>
+          <ErrorBoundary>
+            <AuthInfo />
+          </ErrorBoundary>
+        </ThemeContextProvider>
+      </ErrorBoundary>
     </>
   );
 }
