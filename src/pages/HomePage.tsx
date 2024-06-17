95dea600751565f7a28f1634185029b76b71cef4
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-06-17 20:17:37 +0200

    feat: auth credentials tests

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,10 @@
+import { AuthCredentials } from "../components/Auth/AuthCredentials";
+
 export const HomePage = () => {
-  return <h1 className="dark:text-slate-300">Hello from home!</h1>;
+  return (
+    <div>
+      <h1 className="dark:text-slate-300">Hello from home!</h1>
+      <AuthCredentials />
+    </div>
+  );
 };
